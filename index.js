cf8c8fbdbec7bb9ef252c610c0d5222cbf900ab7
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-08-19 16:10:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 const currentUser = 'Grace Hopper';
      
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.uppercase();
-
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
